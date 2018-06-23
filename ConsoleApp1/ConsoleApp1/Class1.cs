72b7ada8f0073a8d2ee255959c95f50a8db4cc0b
Khaled <khaledassaf2811@gmail.com>
2018-06-23 10:14:50 +1000

    Update 1

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Math;
+
+namespace TeleprompterConsole
+{
+    internal class TelePrompterConfig
+    {
+        private object lockHandle = new object();
+        public int DelayInMilliseconds { get; private set; } = 200;
+
+        public void UpdateDelay(int increment) // negative to speed up
+        {
+            var newDelay = Min(DelayInMilliseconds + increment, 1000);
+            newDelay = Max(newDelay, 20);
+            lock (lockHandle)
+            {
+                DelayInMilliseconds = (int)newDelay;
+            }
+        }
+
+        private object Max(object newDelay, int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private object Min(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Done { get; private set; }
+
+        public void SetDone()
+        {
+            Done = true;
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,17 +1,88 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace TeleprompterConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Khaled!");
-            Console.ReadKey();
+            //var lines = ReadFrom("sampleQuotes.txt");
+            //foreach (var line in lines)
+            //{
+            //    Console.Write(line);
+            //    if (!string.IsNullOrWhiteSpace(line))
+            //    {
+            //        var pause = Task.Delay(100);
+            //        // Synchronously waiting on a task is an
+            //        // anti-pattern. This will get fixed in later
+            //        // steps.
+            //        pause.Wait();
+            //    }
+            //}
+            ShowTeleprompter().Wait();
+        }
+
+        private static async Task ShowTeleprompter()
+        {
+            var words = ReadFrom("sampleQuotes.txt");
+            foreach (var word in words)
+            {
+                Console.Write(word);
+                if (!string.IsNullOrWhiteSpace(word))
+                {
+                    await Task.Delay(200);
+                }
+            }
+        }
+
+        private static async Task GetInput()
+        {
+            var delay = 200;
+            Action work = () =>
+            {
+                do
+                {
+                    var key = Console.ReadKey(true);
+                    if (key.KeyChar == '>')
+                    {
+                        delay -= 10;
+                    }
+                    else if (key.KeyChar == '<')
+                    {
+                        delay += 10;
+                    }
+                } while (true);
+            };
+            await Task.Run(work);
+        }
+
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            using (var reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var words = line.Split(' ');
+                    var lineLength = 0;
+                    foreach (var word in words)
+                    {
+                        yield return word + " ";
+                        lineLength += word.Length + 1;
+                        if (lineLength > 70)
+                        {
+                            yield return Environment.NewLine;
+                            lineLength = 0;
+                        }
+                    }
+                    yield return Environment.NewLine;
+                }
+            }
         }
     }
 }
